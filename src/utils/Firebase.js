e9da08c837f74d4cc32376195060cfd7bc5524b3
Hector <hector@ignisoftware.com>
2020-05-20 19:27:07 -0600

    Updated Firebase

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,42 +1,34 @@
+import Vue from 'vue'
 import { firestorePlugin } from 'vuefire'
-
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/storage'
 
-import EventBus from '../Eventbus'
+Vue.use(firestorePlugin)
 
-const fBase = window.Drupal.settings.m6_platform.f_base
 
-firebase.initializeApp(fBase.config)
+export let db = {}
+export let storage = {}
+export const initFirebase = () => {
+  firebase.initializeApp(window.Drupal.settings.m6_platform.f_base.config)
+  function init() {
+    // Authenticate through token
+    firebase
+      .auth()
+      .signInWithCustomToken(window.Drupal.settings.m6_platform.f_base.token)
+      .then()
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.log('Error: ', error)
 
-function init() {
-  // Authenticate through token
-  firebase
-    .auth()
-    .signInWithCustomToken(fBase.token)
-    .then(() => EventBus.$emit('loaded:firebase'))
-    .catch(function (error) {
-      // eslint-disable-next-line no-console
-      console.log('Error: ', error)
+        setTimeout(init, 500)
+      })
+    // eslint-disable-next-line no-console
+    console.log('Firebase Ready')
+  }
 
-      setTimeout(init, 500)
-    })
-}
-
-init()
-
-const install = Vue => {
-  Vue.use(firestorePlugin)
-}
-
-export const db = firebase.firestore()
-export const storage = firebase.storage()
-
-export default install
-
-// Install by default if included from script tag
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(install)
+  init()
+  db = firebase.firestore()
+  storage = firebase.storage()
 }
